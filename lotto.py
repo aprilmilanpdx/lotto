c1bcab75641a331b078b57bce64ddf1c015d22d6
April Milan <108895508+aprilmilanpdx@users.noreply.github.com>
2023-02-23 11:25:59 -0800

    mid working code commit

--- lotto.py
+++ lotto.py
@@ -1,8 +1,6 @@
 import random
-
-# This line creates a set with 6 random numbers
 lottery_numbers = set(random.sample(list(range(22)), 6))
-
+print(lottery_numbers)
 # Here are your players; find out who has the most numbers matching lottery_numbers!
 players = [
     {"name": "Rolf", "numbers": {1, 3, 5, 7, 9, 11}},
@@ -10,6 +8,12 @@ players = [
     {"name": "Anna", "numbers": {13, 14, 15, 16, 17, 18}},
     {"name": "Jen", "numbers": {19, 20, 12, 7, 3, 5}},
 ]
+
+for player in players:
+  player_numbers = (player["numbers"])
+  # print(player_numbers)
+  numbers_matched = lottery_numbers.intersection(player_numbers)
+  print(player["name"], numbers_matched, len(numbers_matched))
 
 # Then, print out a line such as "Jen won 1000.".
 # The winnings are calculated with the formula:
